822d27ddda0f2da72b6551619bb15d0b90a6ee90
VIP-PC02\vip <sanyi0127@sina.com>
2018-04-11 17:29:55 +0800

    initial create

---
+++ dotnetcore/QRCodeMain/Controllers/QrCodeController.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using QRCodeMain.Data;
+using QRCodeMain.Models;
+
+namespace QRCodeMain.Controllers
+{
+    public class QrCodeController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public QrCodeController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: QrCode
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.QrCodes.ToListAsync());
+        }
+
+        // GET: QrCode/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var qrCode = await _context.QrCodes
+                .SingleOrDefaultAsync(m => m.QrCodeId == id);
+            if (qrCode == null)
+            {
+                return NotFound();
+            }
+
+            return View(qrCode);
+        }
+
+        // GET: QrCode/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: QrCode/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("QrCodeId")] QrCode qrCode)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(qrCode);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(qrCode);
+        }
+
+        // GET: QrCode/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var qrCode = await _context.QrCodes.SingleOrDefaultAsync(m => m.QrCodeId == id);
+            if (qrCode == null)
+            {
+                return NotFound();
+            }
+            return View(qrCode);
+        }
+
+        // POST: QrCode/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("QrCodeId")] QrCode qrCode)
+        {
+            if (id != qrCode.QrCodeId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(qrCode);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!QrCodeExists(qrCode.QrCodeId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(qrCode);
+        }
+
+        // GET: QrCode/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var qrCode = await _context.QrCodes
+                .SingleOrDefaultAsync(m => m.QrCodeId == id);
+            if (qrCode == null)
+            {
+                return NotFound();
+            }
+
+            return View(qrCode);
+        }
+
+        // POST: QrCode/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var qrCode = await _context.QrCodes.SingleOrDefaultAsync(m => m.QrCodeId == id);
+            _context.QrCodes.Remove(qrCode);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool QrCodeExists(int id)
+        {
+            return _context.QrCodes.Any(e => e.QrCodeId == id);
+        }
+    }
+}
--- kickstart.md
+++ kickstart.md
@@ -1,3 +1,9 @@
 
 - create project
 dotnet new mvc --auth Individual
+
+- routes
+  /code/my/1
+  
+- db
+There are many third party tools you can download to manage and view a SQLite database. The image below is from [DB Browser for SQLite](http://sqlitebrowser.org/). If you have a favorite SQLite tool, leave a comment on what you like about it.
